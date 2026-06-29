50192016b1133810f7657a990171e0312692a90c
Mohamudomar <mmohamed020@student.sdccd.edu>
2026-06-28 18:00:56 -0700

    Add assembly program to print a message

---
+++ first.asm
@@ -0,0 +1,16 @@
+section .data
+    msg db "I came,", 10, "I saw,", 10, "I conquered.", 10
+    len equ $ - msg
+
+section .text
+    global _start
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, len
+    int 0x80
+
+    mov eax, 1
+    int 0x80
